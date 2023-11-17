b0e6f741bb039874f39387f52f6fe2ac575906d8
TijanAyo <ayomidetijani6@gmail.com>
2023-11-17 14:56:42 +0100

    Added and create user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser, Position } from '../interface';
+
+const UserSchema: Schema<IUser> = new Schema<IUser>({ 
+    fullName: { 
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    isEmailVerified: { 
+        type: Boolean,
+        default: false 
+    },
+    picture: { type: String },
+    position: { 
+        type: String, 
+        enum: Position 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    }
+},
+    { timestamps: true }
+)
+
+const user = mongoose.model<IUser>('User', UserSchema);
+export default user;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
 import app from './index';
+import  { logger } from './helper/logger';
+import { connect } from 'mongoose';
 
 const PORT = process.env.PORT || 4040;
+const isLocal = process.env.NODE_ENV === 'Develop';
+const url = isLocal ? 'mongodb://127.0.0.1:27017/FUBA' : process.env.MONGO_URI || '';
+
+const connectDB = async (): Promise<void> => {
+    const connectionMessage: string = isLocal ? 'Local 🛠️🛠️' : 'Prod🌐🚀';
+    try {
+      await connect(url);
+      logger.info(`Connected to MongoDB ${connectionMessage}`);
+    } catch (err: any) {
+      logger.error(`Error connecting to mongodb ${err.message}`);
+      process.exit(1);
+    }
+  };
 
 app.listen(PORT, () => {
-  console.info(`Local server running on: http://localhost:${PORT}`);
+  logger.info(`Local server running on: http://localhost:${PORT}`);
+  connectDB();
 });
