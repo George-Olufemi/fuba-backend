446072865db5f66f4acd50e36ee25a46f5036488
TijanAyo <ayomidetijani6@gmail.com>
2023-12-15 10:18:42 +0100

    Change: Replace dummy link with actual url

--- src/controllers/Auth/verifyEmail.controller.ts
+++ src/controllers/Auth/verifyEmail.controller.ts
@@ -9,15 +9,14 @@ class VerifyEmailController {
   public async verifyUserEmail(req: Request, res: Response) {
     const emailVerificationFailPlaceholder: string =
       'https://global.discourse-cdn.com/auth0/original/3X/9/e/9e5239a51247ab46defa4564bacdc319efb6fb5d.png';
-    const emailVerificationSuccessPlaceholder: string =
-      'https://supertokens.com/img/email-verification-success.png';
+    const emailVerificationSuccessPlaceholder: string = 'https://fuba.vercel.app/login';
     try {
       const token = req.params.userToken;
       const response = await verifyEmailService.verifyUserEmail(token);
       if (response !== true) {
         return res.status(403).redirect(emailVerificationFailPlaceholder); // TODO: Use FUBA email not verified page
       } else {
-        return res.status(200).redirect(emailVerificationSuccessPlaceholder); // TODO: Redirect user to login page
+        return res.status(200).redirect(emailVerificationSuccessPlaceholder);
       }
     } catch (err: any) {
       return await customErrorHandler.handleCustomError(err, res);
