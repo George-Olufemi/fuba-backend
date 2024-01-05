8fa4b26560757ec75f290708447fcf99e8d75d7a
TijanAyo <ayomidetijani6@gmail.com>
2024-01-05 08:14:57 +0100

    Added profile to index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './Auth/signin';
 export * from './Auth/verifyEmail';
 export * from './Auth/signUpAsLearner';
 export * from './Auth/signUpAsTutor';
+
+export * from './Account/profile'
