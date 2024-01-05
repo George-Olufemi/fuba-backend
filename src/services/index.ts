76fd2017421a559b3f2a092b75de8288c9dddaf5
TijanAyo <ayomidetijani6@gmail.com>
2024-01-05 08:20:18 +0100

    Remove unnecessary whitespace in services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,4 @@ export * from './Auth/signin';
 export * from './Auth/verifyEmail';
 export * from './Auth/signUpAsLearner';
 export * from './Auth/signUpAsTutor';
-
-export * from './Account/profile'
+export * from './Account/profile';
