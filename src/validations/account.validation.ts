bcab265c0b85741944dced19e56e6bfb36cfd98c
TijanAyo <ayomidetijani6@gmail.com>
2024-04-05 17:44:13 +0100

    change

--- src/validations/account.validation.ts
+++ src/validations/account.validation.ts
@@ -5,7 +5,7 @@ export const editProfileSchema = joi.object({
   phoneNumber: joi.string().lowercase().max(20),
   bio: joi.string().lowercase().max(500),
   address: joi.object({
-    country: joi.string().lowercase().max(20),
+    country: joi.string().lowercase().max(200),
     postalCode: joi.string().lowercase().max(6),
     state: joi.string().lowercase().max(20),
   }),
